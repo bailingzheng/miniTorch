3c55a90773b07d3960939e761aa23ca0e332ae95
Bailing Zheng <bzheng@nvidia.com>
2024-01-06 13:30:08 +0800

    Flattens a contiguous range of dims into a tensor.

--- nn/modules/__init__.py
+++ nn/modules/__init__.py
@@ -1,9 +1,11 @@
 from .linear import Linear
 from .batchnorm import BatchNorm1d
 from .sparse import Embedding
+from .flatten import Flatten
 
 __all__ = [
     'BatchNorm1d',
     'Embedding',
+    'Flatten',
     'Linear',
 ]
---
+++ nn/modules/flatten.py
@@ -0,0 +1,27 @@
+# torch.nn.Flatten(start_dim=1, end_dim=-1)
+
+__all__ = [
+    'Flatten'
+]
+
+class Flatten:
+    """Flattens a contiguous range of dims into a tensor. 
+
+    Parameters
+        start_dim (int) - first dim to flatten (default = 1).
+        end_dim (int) - last dim to flatten (default = -1).
+
+    Shape
+        (*, S_start, ..., S_end, *) -> (*, S_start * ... * S_end, *)
+    """
+
+    def __init__(self, start_dim=1, end_dim=-1):
+        self.start_dim = start_dim
+        self.end_dim = end_dim
+
+    def __call__(self, x):
+       return x.flatten(self.start_dim, self.end_dim)
+
+    def parameters(self):
+        ps = []
+        return ps
